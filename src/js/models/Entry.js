4777d834011b542470db6d683b9069842875936f
Sama Mushtaq <sama.mushtaq@rgare.com>
2019-10-30 00:06:00 -0500

    updated the structure of the Entry class
    modified constructor and class methods

--- src/js/models/Entry.js
+++ src/js/models/Entry.js
@@ -1,32 +1,31 @@
 import uniqid from 'uniqid';
 
 export default class Entry {
-    constructor(title, date, desc, category, duration) {
-        this.id = uniqid();
-        this.title = title;
-        this.date = date;
-        this.desc = desc;
-        this.category = category;
-        this.duration = duration;
-
+    constructor() {
+        this.entries = [];
     }
 
     //to add a new entry to our data storage
-    addNewEntry() {
-        //this function will add the newly created entry to the memory
-        //we will use localStorage for persistence
-
-        //TODO:
-        //make sure that the id we are saving is not present already
-
-        //change the incoming duration to minutes
+    addNewEntry(title, date, desc='', category, duration) {
+        const entry = {
+            id: uniqid(),
+            title,
+            date,
+            desc,
+            category,
+            duration
+        }
+        this.entries.push(entry);
+        return entry;
     }
 
     //to delete a new entry from our data storage
     deleteEntry(id) {
+        const index = this.entries.findIndex(el => el.id === id);
+        this.entries.splice(index, 1);
 
     }
 
-   
+
 
 }
