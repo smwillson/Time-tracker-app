45ab5cac44673dc51861b79012e55cfb72d6638c
Sama Mushtaq <sama.mushtaq@rgare.com>
2019-10-30 00:07:40 -0500

    added event handler for delete button

--- src/js/index.js
+++ src/js/index.js
@@ -25,30 +25,56 @@ const state = {};
  */
 const controlEntry = () => {
 
+    if (!state.dailyEntries) {
+        //add new entry object to the state
+        state.dailyEntries = new Entry();
+    }
+    //create a new entry
     if (entryView.validateForm()) {
 
-        //if all the user entries are valid create a new time entry object
-
-        state.entry = new Entry(
+        //Create a new entry and add it to our global state
+        state.dailyEntries.addNewEntry(
             entryView.getTitle(),
             entryView.getDate(),
             entryView.getDesc(),
             entryView.getCategory(),
-            entryView.getTime()
-        );
-        window.e = state.entry;
+            entryView.getTime());
+
+        //update the UI
+        state.dailyEntries.entries.forEach(entry => {          
+            entryView.createEntry(entry.id, entry.title, entry.duration)
+        });
+        //entryView.createEntry(newEntry);
+
+        //clear the fields
+        entryView.clearForm();
     }
 };
 
 
-//Setting up event listener
+//Setting up event listeners
 
+//element handler for form submission
 elements.submitBtn.addEventListener('click', e => {
     e.preventDefault();
     controlEntry();
 
 });
 
+//Event handler for deletion
+elements.table.addEventListener('click', event => {
+
+    const id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+    //remove the entry from the UI
+    if (event.target.matches('.entry-del-btn , .entry-del-btn *')) {
+        const rowNum = event.target.parentNode.parentNode.parentNode.parentNode.rowIndex;
+        elements.table.deleteRow(rowNum);
+    }
+
+    //now remove it from the state
+    state.dailyEntries.entries.deleteEntry(id);
+
+});
 
 
 //window.e = state.Entry;
