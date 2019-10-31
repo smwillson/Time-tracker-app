7795e39459514976b4a6003906eb6492c5bab325
Sama Mushtaq <sama.mushtaq@rgare.com>
2019-10-31 12:55:34 -0500

    added more html elements

--- src/js/views/common.js
+++ src/js/views/common.js
@@ -4,11 +4,13 @@ export const elements = {
     date: document.querySelector('#time-entry-date'),
     description: document.querySelector('#time-entry-description'),
     category: document.querySelector('#time-entry-tag'),
-    categoryOptions:document.getElementById('projects'),
+    categoryOptions: document.getElementById('projects'),
     hours: document.querySelector('#time-entry-hour'),
     minutes: document.querySelector('#time-entry-mins'),
     table: document.querySelector('#entry-table'),
-    entriesTable : document.querySelector('#daily_item_entries'),
+    entriesTable: document.querySelector('#daily_item_entries'),
     submitBtn: document.querySelector('#btn-submit'),
-    cancelBtn: document.querySelector('#btn-cancel')
+    cancelBtn: document.querySelector('#btn-cancel'),
+    pieChart: document.querySelector('#piechart_3d'),
+    reportSection: document.querySelector('#daily-report-section')
 };
