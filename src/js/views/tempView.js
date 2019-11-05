f676df76e4d8646d430f81a23247c7667cb16877
Sama Mushtaq <sama.mushtaq@rgare.com>
2019-11-04 19:24:10 -0600

    added helper methods for temp conversion

--- src/js/views/tempView.js
+++ src/js/views/tempView.js
@@ -6,14 +6,14 @@ export const renderTemp = (cur, min, max, humidity, winsp, windir, abbr) => {
     const markup = `
     <div class="col-md-6 weather shadow">
                         <div id="weather-icon"><img src="https://www.metaweather.com/static/img/weather/${abbr}.svg"></img></div>
-                        <label>CURRENTLY&nbsp;&nbsp;&nbsp;<strong id="current-temp">${(cur === null) ? cur = '--' : cur}&deg;</strong></label>
+                        <label>CURRENTLY&nbsp;&nbsp;&nbsp;<strong id="current-temp" class="temp">${(cur === null) ? cur = '--' : cur}&deg;</strong></label>
                     </div>
                     <div class="col-md-6 weather shadow">
                         <div class="weather-display">
                             <span>
-                                <label>LOW &nbsp;&nbsp;<strong id="min-temp">${(min === null) ? min = '--' : min}&deg;</strong></label>
+                                <label>LOW &nbsp;&nbsp;<strong id="min-temp" class="temp">${(min === null) ? min = '--' : min}&deg;</strong></label>
                                 <label>&nbsp;&nbsp;&nbsp;<i class="fas fa-caret-right"></i></label>
-                                <label>&nbsp;&nbsp;HIGH &nbsp;&nbsp;<strong id="max-temp">${(max === null) ? max = '--' : max}&deg;</strong></label>
+                                <label>&nbsp;&nbsp;HIGH &nbsp;&nbsp;<strong id="max-temp" class="temp">${(max === null) ? max = '--' : max}&deg;</strong></label>
                             </span>
                             <div>
                                 <i class="fas fa-umbrella"></i>&nbsp;&nbsp;&nbsp;<span>HUMIDITY&nbsp;
@@ -26,4 +26,19 @@ export const renderTemp = (cur, min, max, humidity, winsp, windir, abbr) => {
                         </div>
     </div>`
     elements.temperature.insertAdjacentHTML('beforeend', markup);
+}
+
+export const toggleCheckBox = (state) => {
+    elements.weatherChkBox.disabled = state;
+}
+
+export const convertTempType = (temp, type) => {
+    temp = parseInt(temp, 10);
+    let newTemp;
+    if (type === 'F') {
+        newTemp = (temp * 9 / 5) + 32;
+    } else {
+        newTemp = (temp - 32) * 5 / 9;
+    }
+    return Math.round(newTemp);
 }
